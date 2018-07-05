c3eb34a8c3b8a87a3b8c0895f2161a9aa7eb6c7c
Shea Lewis <shea.lewis89@gmail.com>
2018-07-05 09:32:10 -0700

    The great purge

--- src/support/helpers.js
+++
@@ -1,9 +0,0 @@
-export function addClassModifiers(base, modifiers = []) {
-    if (! Array.isArray(modifiers)) {
-        modifiers = modifiers.split(' ');
-    }
-
-    modifiers = modifiers.map(modifier => `${base}--${modifier}`);
-
-    return [base, ...modifiers];
-}
